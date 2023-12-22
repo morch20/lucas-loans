0624d6f38f46d5b871a2f554177239d13e89722e
Joseph <josephmt1176@gmail.com>
2023-12-21 19:24:57 -0500

    added dots to text

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,7 +7,7 @@ const values: IQuestion[] = [
         value: "MI",
         question: "Monthly income before taxes.",
         subtext:
-            "Please put in the amount you make per month before taxes",
+            "Please put in the amount you make per month before taxes.",
         type: "text",
         placeholder: "Enter monthly income...",
         icon: MdAttachMoney,
@@ -33,7 +33,7 @@ const values: IQuestion[] = [
     {
         value: "DP",
         question: "Down payment",
-        subtext: "3-5% minimum for conventional loans and 3.5% for FHA",
+        subtext: "3-5% minimum for conventional loans and 3.5% for FHA.f",
         type: "text",
         placeholder: "Enter down payment...",
         icon: TbPigMoney ,
