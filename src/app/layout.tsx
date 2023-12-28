34ae673ce98f2150e2548f46e2e5e172870c3210
Joseph <josephmt1176@gmail.com>
2023-12-28 12:15:07 -0500

    Removed head from layout & added scripts to home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Head from "next/head";
 
 export const metadata: Metadata = {
     title: "Lucas Loans",
@@ -16,16 +15,6 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <Head>
-                <script
-                    src="//widget.manychat.com/686044_35370.js"
-                    defer={true}
-                ></script>
-                <script
-                    src="https://mccdn.me/assets/js/widget.js"
-                    defer={true}
-                ></script>
-            </Head>
             <body className="bg-[#eff4f7] text-tertiary w-full h-full flex flex-col items-center">
                 <Navbar />
                 <div className="container w-full h-full px-5 sm:px-7 overflow-x-clip">
