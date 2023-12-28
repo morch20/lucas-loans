e899bc9e2fec7cde8054426fbc77d09c668941e1
Joseph <josephmt1176@gmail.com>
2023-12-28 12:02:16 -0500

    Added head tag to test widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 export const metadata: Metadata = {
     title: "Lucas Loans",
@@ -15,6 +16,16 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            <Head>
+                <script
+                    src="//widget.manychat.com/686044_35370.js"
+                    defer={true}
+                ></script>
+                <script
+                    src="https://mccdn.me/assets/js/widget.js"
+                    defer={true}
+                ></script>
+            </Head>
             <body className="bg-[#eff4f7] text-tertiary w-full h-full flex flex-col items-center">
                 <Navbar />
                 <div className="container w-full h-full px-5 sm:px-7 overflow-x-clip">
