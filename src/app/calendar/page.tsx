117c21fe29c316142febe9bfa36dc3a879c8f5dd
Joseph <josephmt1176@gmail.com>
2024-01-02 19:51:13 -0500

    fixing style of iframe

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -2,15 +2,13 @@ import Script from "next/script";
 
 const Calendar = () => {
     return (
-        <div className="w-full h-[100dvh] py-5 md:py-10">
+        <div className="min-h-screen mt-10 flex h-fit w-full">
             <iframe 
                 src="https://api.leadconnectorhq.com/widget/booking/Y0fBPK62Ip9kLvW40dsf"  
-                className="w-full h-full border-none "
-                style={{overflow: "hidden"}}
+                className=" w-full flex-grow border-none overflow-hidden "
                 // scrolling="no" 
                 id="Y0fBPK62Ip9kLvW40dsf_1704239607905">
             </iframe>
-            <br />
             <Script src="https://link.msgsndr.com/js/form_embed.js" type="text/javascript"></Script>
         </div>
     );
