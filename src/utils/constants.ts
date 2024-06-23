b3a8bf6defe0aeec0c77d764d4fc1f38739acd04
Joseph <josephmt1176@gmail.com>
2024-06-23 18:33:24 -0400

    added steps constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,12 +2,11 @@ import { IQuestion } from "@/Interfaces";
 import { MdCreditScore, MdPayments, MdAttachMoney } from "react-icons/md";
 import { TbPigMoney } from "react-icons/tb";
 
-const values: IQuestion[] = [
+export const values: IQuestion[] = [
     {
         value: "MI",
         question: "Monthly income before taxes.",
-        subtext:
-            "Please put in the amount you make per month before taxes.",
+        subtext: "Please put in the amount you make per month before taxes.",
         type: "tel",
         placeholder: "Enter monthly income...",
         icon: MdAttachMoney,
@@ -19,7 +18,7 @@ const values: IQuestion[] = [
             "Include car loans, credit card payments, personal loans and other mortgages.",
         type: "tel",
         placeholder: "Enter monthly debt...",
-        icon: MdPayments ,
+        icon: MdPayments,
     },
     {
         value: "CS",
@@ -28,18 +27,68 @@ const values: IQuestion[] = [
         type: "tel",
         placeholder: "Enter credit score...",
         limit: 850,
-        icon: MdCreditScore ,
+        icon: MdCreditScore,
     },
     {
         value: "DP",
         question: "Down payment",
-        subtext: "3-5% minimum for conventional loans and 3.5% for FHA.\n*Please put a dollar amount*",
+        subtext:
+            "3-5% minimum for conventional loans and 3.5% for FHA.\n*Please put a dollar amount*",
         type: "tel",
         placeholder: "Enter down payment...",
-        icon: TbPigMoney ,
-    }
+        icon: TbPigMoney,
+    },
 ];
 
-const emailValidator = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+export const emailValidator = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-export { values, emailValidator };
+export const howToBuyAHouseSteps = [
+    {
+        title: "Assess Your Finances",
+        description:
+            "Start by reviewing your credit score and financial situation. Determine how much you can comfortably afford, factoring in all potential costs like the down payment, closing fees, and monthly mortgage payments.",
+        imageUrl: "/howToBuyAHouse/assessYourFinances.svg",
+    },
+    {
+        title: "Get Preapproved for a Mortgage",
+        description:
+            "Connect with us to find the best mortgage rates and terms. Our experts will help you gather the necessary documents to get preapproved, giving you a clear picture of your borrowing power.",
+        imageUrl: "/howToBuyAHouse/getPreapprovedForAMortgage.svg",
+    },
+    {
+        title: "Choose Your Real Estate Agent",
+        description:
+            "Pick an agent who knows your desired neighborhood and can provide valuable insights. A great agent will be your guide through the complexities of the market.",
+        imageUrl: "/howToBuyAHouse/chooseYourRealEstateAgent.svg",
+    },
+    {
+        title: "Begin House Hunting",
+        description:
+            "List your priorities and preferences for your new home. Visit open houses and schedule showings to find the perfect match.",
+        imageUrl: "/howToBuyAHouse/beginHouseHunting.svg",
+    },
+    {
+        title: "Make a Competitive Offer",
+        description:
+            "With your agent's help, craft an offer that stands out in the current market conditions. Be ready for potential negotiations to get the best deal.",
+        imageUrl: "/howToBuyAHouse/makeACompetitiveOffer.svg",
+    },
+    {
+        title: "Conduct a Home Inspection",
+        description:
+            "Hire a professional to inspect the property thoroughly. Use their report to address any concerns and negotiate repairs or adjustments if needed.",
+        imageUrl: "/howToBuyAHouse/conductAHomeInspection.svg",
+    },
+    {
+        title: "Finalize Your Mortgage",
+        description:
+            "Work closely with us to complete your mortgage application. We'll guide you through locking in your interest rate and submitting any additional documents.",
+        imageUrl: "/howToBuyAHouse/finalizeYourMortgage.svg",
+    },
+    {
+        title: "Close the Deal",
+        description:
+            "Review and sign all necessary closing documents. Pay your down payment and closing costs, and then receive the keys to your new home!",
+        imageUrl: "/howToBuyAHouse/closeTheDeal.svg",
+    },
+];
