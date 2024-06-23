2151c6cdbe7c83eb864f2949f41b831de2ddf5a6
Joseph <josephmt1176@gmail.com>
2024-06-23 15:53:29 -0400

    Added Accordion component from Shadcn

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
+
 export function pause(delay = 200) {
     return new Promise((res) => setTimeout(res, delay));
 }
